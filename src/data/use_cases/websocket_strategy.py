ef8fd33bb7241a76ce660cf54c5b2da4dd47f035
Marquezv <vmarquezv@outlook.com.br>
2024-06-03 17:55:57 -0300

    feat: Send messages to WebSocket to MQTT

--- src/data/use_cases/websocket_strategy.py
+++ src/data/use_cases/websocket_strategy.py
@@ -1,8 +1,17 @@
+from paho.mqtt.client import Client as mqttc
 from src.domain.use_cases.message_strategy import MessageStrategy
+from src.presentation.session_payload import SessionPayload
 
 class WebSocketStrategy(MessageStrategy):
 
     @classmethod
-    def handle_message(self, message):
-        print("Handling WebSocket message")
+    def handle_message(self, session_payload: SessionPayload, connection: mqttc):
+        
+        session = session_payload.session
+        
+        session.device_id = "cloud_01"
 
+        print("Handling WebSocket message")
+        topic = "/{}/{}/{}".format(session.device_id, session.session_id[0], session.action[0])
+        connection.publish(topic, session_payload.package())
+        return session_payload
